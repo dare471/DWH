712f2a2e50c960a030b289241320f158575ac0df
Erzhan15 <52883396+Erzhan15@users.noreply.github.com>
2022-05-17 12:40:43 +0600

    max date

--- Erzhan/L1_KONTRAGENTY.sql
+++ Erzhan/L1_KONTRAGENTY.sql
@@ -1,4 +1,37 @@
 /****** Naidi pasxalku :)  ******/
+
+with maxKoi as (
+SELECT [nomer_stroki]
+      ,[tip]
+      ,kik.[vid]
+      ,[predstavlenie]
+      ,[znacheniya_poley]
+      ,[strana]
+      ,[region]
+      ,[gorod]
+      ,[adres_e_p]
+      ,[domennoe_imya_servera]
+      ,[nomer_telefona]
+      ,[nomer_telefona_bez_kodov]
+      ,[vid_dlya_spiska]
+      ,kik.[deystvuet_s]
+      ,[c_r_m___osnovnoy_dlya_svyazi]
+      ,[oblast_dannykh_osnovnye_dannye]
+      ,kik.[ssylka]
+from [L0].[dbo].[tab_kontaktnaya_informatsiya_kontragenty] kik
+join (SELECT 
+      [vid]
+      ,max([deystvuet_s])[deystvuet_s]
+      ,tki.[ssylka]
+FROM [L0].[dbo].[tab_kontaktnaya_informatsiya_kontragenty] tki
+group by [vid]
+,tki.[ssylka]
+) tki
+	on tki.ssylka=kik.ssylka and tki.deystvuet_s=kik.deystvuet_s and tki.vid=kik.vid
+--where kik.ssylka=0xA2337085C2A4312A11EB22397ED3D617
+)
+
+
 SELECT k.[ssylka] as [guid]
 	  ,p.ssylka as [guid_partner]
       ,k.[naimenovanie]
@@ -29,9 +62,11 @@ SELECT k.[ssylka] as [guid]
 
   FROM [L0].[dbo].[kontragenty] k
   left join [L0].[dbo].[partnery] p on k.partner=p.ssylka
-  left join [L0].dbo.[tab_kontaktnaya_informatsiya_kontragenty] kik on k.ssylka=kik.ssylka and kik.vid = 0x80DE000C29E67B2E11E628A23AABB795
+  left join maxKoi kik on k.ssylka=kik.ssylka and kik.vid = 0x80DE000C29E67B2E11E628A23AABB795
   left join [L0].[dbo].biznes_regiony br on p.biznes_region=br.ssylka
   left join [L0].[dbo].polzovateli polz on p.osnovnoy_menedzher=polz.ssylka
+
+
 UNION ALL
 SELECT
 	fl.ssylka AS [guid],
