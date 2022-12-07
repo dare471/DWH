838d1400ef41a38dee14181670110abba74951e3
Erzhan15 <52883396+Erzhan15@users.noreply.github.com>
2022-12-07 12:27:32 +0600

    obnova 07.12.2022

---
+++ Erzhan/L0_recorder.sql
@@ -0,0 +1,293 @@
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document568 AS ¬нутреннееѕотребление“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document778 AS ѕодготовка ѕередачеЌћј WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document861 AS —писаниеќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document620 AS «апись нигиѕокупок WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document828 AS –асходный ассовыйќрдер WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document749 AS ѕередачаƒавальцу WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document756 AS ѕеремещениећатериалов¬ѕроизводстве WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document806 AS ѕриходный ассовыйќрдер WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document577 AS ¬озврат“оваровќт лиента WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89494 AS ѕереоценкаЌћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document751 AS ѕередачаќ—јрендатору WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document790 AS ѕриобретение“оваров”слуг WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document702 AS ќпераци€ѕоѕлатежной арте WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document875 AS —чет‘актураѕолученныйјванс WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document877 AS “аможенна€ƒеклараци€»мпорт WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document791 AS ѕриобретение”слугѕоЋизингу WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89528 AS Ётапѕроизводства2_2 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document727 AS ќтчет омиссионера WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document726 AS ќтчетƒавальцу WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89499 AS ѕодготовка ѕередачеЌћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document804 AS ѕрин€тие ”четуќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document550 AS јмортизаци€Ќћј WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document567 AS ¬несениеƒенежных—редств¬ ассу  ћ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document764 AS –асчет урсовых–азниц WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document789 AS ѕоступление—ырь€ќтƒавальца WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document695 AS Ќачислениеќценочныхќб€зательствѕоќтпускам WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document578 AS ¬озврат“оваровѕоставщику WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89516 AS —писаниеќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89483 AS ќтгрузка“оваров—’ранени€ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89469 AS »зменениеѕараметровќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document824 AS –аспределениеѕроизводственных«атрат WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document854 AS —писаниеЅезналичныхƒенежных—редств WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document873 AS —чет‘актураЌаЌеподтвержденную–еализацию0 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document821 AS –аспределениеƒоходов»–асходовѕоЌаправлени€мƒе€тельности WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document699 AS ќпераци€Ѕух WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document57188 AS ѕеремаркировка“оваров√»—ћ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89459 AS ¬озвратќ—ќтјрендатора2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89480 AS ќпераци€ѕояндекс ассе WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89451 AS јкт¬ыполненных¬нутренних–абот WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document559 AS ¬водќстатков WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89501 AS ѕоступление“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document814 AS ѕрочиеƒоходы–асходы WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89461 AS ¬ыкупѕрин€тыхЌа’ранение“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document869 AS —чет‘актура¬ыданныйјванс WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document57185 AS ћаркировка“оваров√»—ћ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document675 AS  орректировкаЌалогообложени€Ќƒ—ѕартий“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document733 AS ќтчетѕо омиссиићеждуќрганизаци€ми WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89493 AS ѕеремещениеќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document826 AS –аспределение–асходовЅудущихѕериодов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document686 AS ћодернизаци€ќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document777 AS ѕогашение—тоимости“ћ÷¬Ёксплуатации WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document729 AS ќтчет омитенту WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89476 AS ћодернизаци€ќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89514 AS —писаниеЌƒ—Ќа–асходы WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document858 AS —писаниеЌедостач“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document825 AS –аспределениеѕрочих«атрат WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document833 AS –еализаци€”слугѕрочихјктивов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document761 AS ѕеремещение“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document574 AS ¬озврат—ырь€ƒавальцу WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89500 AS ѕодготовка ѕередачеќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89463 AS ƒвижениеѕродукции»ћатериалов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document636 AS «а€влениеќ¬возе“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document679 AS  орректировка–еализации WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89495 AS ѕереоценкаќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document832 AS –еализаци€“оваров”слуг WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document552 AS јмортизаци€ќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89489 AS ќтчетќператора—истемыѕлатон WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document779 AS ѕодготовка ѕередачеќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document784 AS ѕоступлениејрендованныхќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document723 AS ќтражение«арплаты¬‘инансовом”чете WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89455 AS ¬водќстатков¬необоротныхјктивов2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document765 AS ѕереоценкаЌћј WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document752 AS ѕередача—ырь€ѕереработчику WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document725 AS ќтчетЅанкаѕоќпераци€мЁквайринга WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document560 AS ¬водќстатков¬необоротныхјктивов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document51080 AS »сковое«а€вление WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document674 AS  орректировкаЌазначени€“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document769 AS ѕересортица“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document583 AS ¬ыбытиеƒенежныхƒокументов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document786 AS ѕоступлениеƒенежныхƒокументов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document541 AS јвансовыйќтчет WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89503 AS ѕрин€тие ”четуЌћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document734 AS ќтчетѕо омиссиићеждуќрганизаци€миќ—писании WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document691 AS Ќачислениеƒивидендов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document678 AS  орректировкаѕриобретени€ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document868 AS —чет‘актура¬ыданный WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document730 AS ќтчет омитентуќ—писании WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document732 AS ќтчетѕереработчика WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document586 AS ¬ыкуп¬озвратной“ары лиентом WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document766 AS ѕереоценкаќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89492 AS ѕеремещениеЌћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document590 AS ¬ыработкаЌћј WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89452 AS јмортизаци€Ќћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document792 AS ѕриобретение”слугѕрочихјктивов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document555 AS јннулированиеѕодарочных—ертификатов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document543 AS јкт¬ыполненных–абот WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document731 AS ќтчетќ–озничныхѕродажах WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89515 AS —писаниеЌћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89507 AS –аспределение¬озвратныхќтходов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document656 AS »нвентаризаци€Ќаличныхƒенежных—редств WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89504 AS ѕрин€тие ”четуќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document647 AS »зменениеѕараметровќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document708 AS ќприходование»злишков“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document575 AS ¬озврат—ырь€ќтѕереработчика WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document855 AS —писание«адолженности WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document589 AS ¬ыпускѕродукции WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document576 AS ¬озврат“оваровћеждуќрганизаци€ми WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89517 AS —писаниеѕрин€тыхЌа’ранение“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document848 AS —борка“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89502 AS ѕриемка“оваровЌа’ранение WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document55687 AS ЌачислениеЌалоговѕојгентскому¬ознаграждению WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89506 AS ѕроизводствоЅез«аказа WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document728 AS ќтчет омиссионераќ—писании WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89453 AS јмортизаци€ќ—2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document570 AS ¬озвратћатериалов»зѕроизводства WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89478 AS Ќачисление—писание–езервовѕредсто€щих–асходов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document753 AS ѕередача“оваровћеждуќрганизаци€ми WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89468 AS »зменениеѕараметровЌћј2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document572 AS ¬озвратќ—ќтјрендатора WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document587 AS ¬ыкуп¬озвратной“ары”ѕоставщика WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document842 AS –егламентна€ќпераци€ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document566 AS ¬заимозачет«адолженности WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document595 AS ƒепонирование«арплаты WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document724 AS ќтражение–асхожденийѕри»нкассацииƒенежных—редств WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document830 AS –асчет—ебестоимости“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document874 AS —чет‘актураѕолученный WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document813 AS ѕрочееќприходование“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document579 AS ¬осстановлениеЌƒ—ѕоќбъектамЌедвижимости WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document696 AS Ќачислени€ редитов»ƒепозитов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document831 AS –еализаци€ѕодарочных—ертификатов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document621 AS «апись нигиѕродаж WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document759 AS ѕеремещениеќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document573 AS ¬озвратѕодарочных—ертификатов WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document785 AS ѕоступлениеЅезналичныхƒенежных—редств WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89474 AS  орректировка¬идаƒе€тельностиЌƒ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89491 AS ѕередачаќ—јрендатору2_4 WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document750 AS ѕередачаћатериалов¬ѕроизводство WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document802 AS ѕрин€тие ”четуЌћј WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document89520 AS —чет‘актураѕолученныйЌалоговыйјгент WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document782 AS ѕорча“оваров WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document787 AS ѕоступлениеќтѕереработчика WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document859 AS —писаниеЌћј WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document582 AS ¬ыбытиејрендованныхќ— WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document872 AS —чет‘актураЌалоговыйјгент WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document788 AS ѕоступлениеѕредметовЋизинга WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document585 AS ¬ыемкаƒенежных—редств»з ассы  ћ WHERE _posted = 1
+UNION ALL
+SELECT _IDRRef AS [guid], _Number AS [number] FROM _Document822 AS –аспределениеЌƒ— WHERE _posted = 1
